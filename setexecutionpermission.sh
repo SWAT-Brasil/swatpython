54fd88536a8af7369fc994b228a69acc9fe7895d
jairo <jairo.rotava@gmail.com>
2020-04-23 09:02:30 -0300

    execution script

---
+++ setexecutionpermission.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+chmod +x "$DIR/swat2012rev637/swat2012_rev637_linux"
+chmod +x "$DIR/swat2012rev670/swat2012_rev670_linux"
